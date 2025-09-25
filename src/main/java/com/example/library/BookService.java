2a365966df4a2c934928416165b5929e100a9668
chandusri26 <garikachandusri@gmail.com>
2025-09-25 14:22:37 +0530

    Add issue and return book functionality

--- src/main/java/com/example/library/BookService.java
+++ src/main/java/com/example/library/BookService.java
@@ -3,9 +3,14 @@ package com.example.library;
 import java.util.List;
 
 public interface BookService {
+    // Existing methods
     Book saveBook(Book book);
     List<Book> getAllBooks();
     Book getBookById(Long id);
     Book updateBook(Long id, Book book);
     void deleteBook(Long id);
+
+    // New methods for issue and return
+    void issueBook(Long id);
+    void returnBook(Long id);
 }
